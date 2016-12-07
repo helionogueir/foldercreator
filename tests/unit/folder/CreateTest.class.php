ddfdf35a02609db85210d1f4c5f0653467a0db1a
helionogueir <helio.nogueir@gmail.com>
2016-12-07 14:42:55 -0200

    Start Version

--- tests/unit/folder/CreateTest.class.php
+++ tests/unit/folder/CreateTest.class.php
@@ -7,7 +7,7 @@ use helionogueir\foldercreator\folder\Delete;
 class CreateTest extends TestCase {
 
   public function testMake() {
-    $pathname = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "helionogueir_foldercreator_folder_create;";
+    $pathname = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "helionogueir_foldercreator_folder_create";
     $this->assertTrue((new Create())->mkdir($pathname));
     (new Delete())->rm($pathname);
   }
--- tests/unit/folder/DeleteTest.class.php
+++ tests/unit/folder/DeleteTest.class.php
@@ -7,7 +7,7 @@ use helionogueir\foldercreator\folder\Delete;
 class DeleteTest extends TestCase {
 
   public function testRm() {
-    $pathname = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "helionogueir_foldercreator_folder_delete;";
+    $pathname = sys_get_temp_dir() . DIRECTORY_SEPARATOR . "helionogueir_foldercreator_folder_delete";
     if ((new Create ())->mkdir($pathname)) {
       $this->assertTrue((new Delete())->rm($pathname));
     }
